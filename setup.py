deabeb8bd6d46c48611212a17f6b184335c89c0f
Bob Martens <martenrt@mlc-wels.edu>
2011-03-01 10:41:17 -0600

    initial commit for MLC theme for Plone 4.x

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='plonetheme.mlc',
+      version=version,
+      description="New theme for Martin Luther College",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Bob Martens',
+      author_email='martenrt@mlc-wels.edu',
+      url='http://mlc-wels.edu',
+      license='',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plonetheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
+
