a88f1f40ecfbf7ba366edb74da0aebf30b524964
Bob Martens <martenrt@mlc-wels.edu>
2012-01-13 10:51:18 -0600

    added file redifection python script

---
+++ plonetheme/mlc/skins/templates/file_redirection.py
@@ -0,0 +1,27 @@
+## Script (Python) "file_redirection"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+"""
+Will open file automatically for non-editors.
+"""
+request = context.REQUEST
+response = request.response
+
+from Products.CMFCore.utils import getToolByName
+
+properties = getToolByName(context, 'portal_properties')
+membership = getToolByName(context, 'portal_membership')
+
+download = properties.site_properties.getProperty('redirect_links', False)
+can_modify = membership.checkPermission('Modify portal content', context)
+
+if download and not can_modify:
+    return response.redirect(context.absolute_url())
+else:
+    return context.file_view()
